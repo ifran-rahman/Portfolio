14a1e76260ecc3fc7f1d264642179feee11abc62
Ifran Rahman Nijhum <52383588+ifran-rahman@users.noreply.github.com>
2022-08-19 21:46:13 +0600

    Update main.py

--- main.py
+++ main.py
@@ -170,3 +170,13 @@ st.markdown('''
 ''')
 txt2('Email', 'ifran.nijhum@northsouth.edu')
 txt2('Personal Email', 'ifranrahmannijhum@gmail.com')
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-389CLJYLPS"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-389CLJYLPS');
+</script>
