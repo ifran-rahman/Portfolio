71aca471531b611748535c64131b694bba285dc0
Ifran Rahman Nijhum <52383588+ifran-rahman@users.noreply.github.com>
2022-08-26 22:41:28 +0600

    Update main.py

--- main.py
+++ main.py
@@ -195,7 +195,7 @@ st.markdown('''
 
 txt('**Coordinator**, Team Research & Development, *NSU ACM Student Chapter*','Jan 2020 - Jan 2022')
 st.markdown('''
-- Currently instructing a research project (stated in the project section of this website).
+- Currently instructing a research project (stated in the <a href="#publications-and-projects">project section</a> of this website).
 - Managed an international programming contest “Semi-code”, Inter-University Hackathon "HackNSU Season 3”. 
 - Planned and arranged intra hackathons, programming contests, and team meetings. 
 ''')
