b0da3cfdfc56049a8ee692f45645caac9994e4a7
Ifran Rahman Nijhum <52383588+ifran-rahman@users.noreply.github.com>
2022-10-18 02:20:17 +0600

    Update main.py

--- main.py
+++ main.py
@@ -102,12 +102,12 @@ Doctors can verify each predicted beat. Model gets trained after a certain perio
 <a href="https://github.com/ifran-rahman/Heartisan">code</a> 
 </div>
 ''')
-txt5('images/ecg.png', 
+txt5('images/data_analytics.png', 
 '''
 <div style="text-align: left">
 <b> <a> Customer Sales Data Analysis and Prediction </a> </b> <br>
 Analyze customer sales data using Google Looker Studio (Data Studio). Used XGBoost Regressor to predict customer's order quantity. <br>
-<a href="https://datastudio.google.com/reporting/8d8f0ff8-b3b2-4ec2-8556-f095e695c55e">Dashboard</a> 
+<a href="https://datastudio.google.com/reporting/8d8f0ff8-b3b2-4ec2-8556-f095e695c55e">dashboard</a> &nbsp;
 <a href="https://github.com/ifran-rahman/Customer_Data_Analysis">code</a> 
 </div>
 ''')
