f2e22ff30333216842f4dd09e9d518cff3659abf
Ifran Rahman Nijhum <52383588+ifran-rahman@users.noreply.github.com>
2023-04-16 04:20:15 +0600

    Update main.py

--- main.py
+++ main.py
@@ -136,13 +136,14 @@ txt5('https://github.com/ifran-rahman/Heartisan/blob/main/images/incremental%20l
 st.markdown('''
 ### Projects 
 (_On Going_)
-txt5('images/drkd.png', 
+txt5('https://www.researchgate.net/publication/350720259/figure/fig2/AS:1010160138989570@1617852353760/Flower-Android-client-architecture.ppm', 
 '''
 <div style="text-align: left">
 <b> <a> Data Secured Biomedical Software </a> </b> <br>
 Working on building the federated learning and machine learning pipeline. 
 </div>
 ''')
+
 (_Previous_)
 txt5('images/drkd.png', 
 '''
