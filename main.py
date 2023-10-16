bca935a8a779722b82a09b63a024fb684b9f8a75
Ifran Rahman Nijhum <52383588+ifran-rahman@users.noreply.github.com>
2023-10-16 23:08:16 +0100

    modify image link

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ st.set_page_config(
 )
 with open("style.css") as f:
     st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
-image = Image.open('https://github.com/ifran-rahman/Heartisan/blob/main/images/dp-2.png')
+image = Image.open('https://github.com/ifran-rahman/Portfolio/blob/main/images/dp-2.png')
 
 #####################
 # Header 
