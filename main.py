2241d6a6e98869652dba45f0d6a72b20fa06f484
Ifran Rahman Nijhum <52383588+ifran-rahman@users.noreply.github.com>
2024-06-15 00:41:10 +0600

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,7 @@ txt5('images/vispol.png',
 </div>
 ''')
 
-txt5('images/vispol.png', 
+txt( 
 '''
 <div style="text-align: left">
 <b> <a href="https://ieeexplore.ieee.org/document/9666654"> Visual Pollution Detection Using Google Street View and YOLO </a> </b> <br>
